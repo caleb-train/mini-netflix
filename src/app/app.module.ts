39c575f96c058c6bb6844e48819e475b607b280b
caleb-42 <ewisco316@yahoo.com>
2019-10-22 03:22:55 +0100

    ft-create auth pages mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
+import { AuthComponent } from './views';
+import { NavBarComponent } from './components';
+
+import { appRoutes } from './routes/app.route';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/app.route.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { AuthComponent } from '../views';
+
+export const appRoutes:Route[] = [
+  {path: '', component: AuthComponent }
+]
